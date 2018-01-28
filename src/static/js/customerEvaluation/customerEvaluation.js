7d72ad3cee2bed353fb948e44a777acfc04bb59e
xiams <410827024@qq.com>
2018-01-28 16:23:09 +0800

    分页

--- src/static/js/customerEvaluation/customerEvaluation.js
+++ src/static/js/customerEvaluation/customerEvaluation.js
@@ -1,3 +1,7 @@
 $(function(){
     console.log('------->customerEvaluation')
+    $('ul.pagination').click(function(e){
+        console.warn(e.target)
+        $(e.target).parent('li.page').addClass('active').siblings().removeClass('active')
+    })
 })
